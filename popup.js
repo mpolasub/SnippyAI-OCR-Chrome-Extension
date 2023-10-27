d98d8ce098b49b6ff7ed6f6c6e49d90a4e5c277e
Matthew Polasub <nondhawit@gmail.com>
2023-10-27 03:39:34 -0700

    created pop u  structure

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("close-button").addEventListener("click", function () {
+        window.close();
+    });
+});
